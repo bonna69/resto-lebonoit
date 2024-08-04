59add3a3016848559c2a18d9e17547bfbb7bb9ea
bonna69 <kaabilobna69@gmail.com>
2024-08-04 21:52:00 +0200

    rectification avis

---
+++ src/Entity/Avis.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AvisRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: AvisRepository::class)]
+class Avis
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nom = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $email = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $commentaire = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): static
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getCommentaire(): ?string
+    {
+        return $this->commentaire;
+    }
+
+    public function setCommentaire(string $commentaire): static
+    {
+        $this->commentaire = $commentaire;
+
+        return $this;
+    }
+}
--- src/Form/AvisType.php
+++ src/Form/AvisType.php
@@ -1,12 +1,15 @@
 <?php
 // src/Form/AvisType.php
+
 namespace App\Form;
 
+use App\Entity\Avis;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class AvisType extends AbstractType
@@ -16,25 +19,26 @@ class AvisType extends AbstractType
         $builder
             ->add('nom', TextType::class, [
                 'label' => 'Nom',
-                'required' => true,
+                'attr' => ['placeholder' => 'Votre nom']
             ])
-            ->add('email', TextType::class, [
+            ->add('email', EmailType::class, [
                 'label' => 'Email',
-                'required' => true,
+                'attr' => ['placeholder' => 'Votre email']
             ])
             ->add('commentaire', TextareaType::class, [
-                'label' => 'Commentaire',
-                'required' => true,
+                'label' => 'Avis',
+                'attr' => ['placeholder' => 'Votre avis']
             ])
             ->add('submit', SubmitType::class, [
-                'label' => 'Soumettre',
+                'label' => 'Envoyer',
+                'attr' => ['class' => 'submit-button']
             ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            // Configure your form options here
+            'data_class' => Avis::class,
         ]);
     }
 }
